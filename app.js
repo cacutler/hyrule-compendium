5617f85b038618fb0d40b255d7db4378d1a33bee
Elmer Richter <elmer.richter@utahtech.edu>
2024-06-14 10:11:51 -0600

    template

---
+++ app.js
@@ -0,0 +1,12 @@
+Vue.createApp({
+    data() {
+      return {
+        titleh1: "Template",
+      };
+    },
+    methods: {},
+    created: function () {
+      console.log("vue app loaded!");
+    },
+  }).mount("#app");
+  
